fbb00bd82fa350c4a6c65dda20b82ddcb50430d4
Norman Maurer <norman_maurer@apple.com>
2017-08-04 19:52:01 +0200

    Fix incorrect javadocs in Http2RemoteFlowController
    Motivation:
    
    The javadocs of Http2RemoteFlowController.isWritable(...) are incorrect.
    
    Modifications:
    
    Update javadocs to reflect reality.
    
    Result:
    
    Correct javadocs.

--- codec-http2/src/main/java/io/netty/handler/codec/http2/Http2RemoteFlowController.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/Http2RemoteFlowController.java
@@ -67,11 +67,11 @@ public interface Http2RemoteFlowController extends Http2FlowController {
     /**
      * Determine if the {@code stream} has bytes remaining for use in the flow control window.
      * <p>
-     * Note that this only takes into account HTTP/2 flow control. It does <strong>not</strong> take into account
-     * the underlying {@link io.netty.channel.Channel#isWritable()}.
+     * Note that it will take into account channel writability, and so will return {@code false} if
+     * {@link io.netty.channel.Channel#isWritable()} returns {@code false} as well.
      * @param stream The stream to test.
-     * @return {@code true} if if the {@code stream} has bytes remaining for use in the flow control window.
-     * {@code false} otherwise.
+     * @return {@code true} if the {@code stream} has bytes remaining for use in the flow control window and the
+     * channel is writable, {@code false} otherwise.
      */
     boolean isWritable(Http2Stream stream);
 
